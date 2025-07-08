9ad2dbc8e8672c3e483b78c5e82e93df8c01a3e7
NammanShukla <nammanshukla262@gmail.com>
2025-07-08 18:08:37 +0530

    Add uploadController for file uploads using multer and update UserSchema to include avatar field

---
+++ server/controllers/uploadController.js
@@ -0,0 +1,18 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // create this folder in root
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, `${req.user._id}-${Date.now()}${ext}`);
+  }
+});
+
+const upload = multer({ storage });
+
+module.exports = {
+  upload,
+};
